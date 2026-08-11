241a2e380fd2cfcbebea19685c22a36c8780030b
Rahat Ahmed <87570918+Rahat0764@users.noreply.github.com>
2026-08-12 03:38:03 +0600

    Update config.js

--- api/config.js
+++ api/config.js
@@ -4,6 +4,7 @@ module.exports = (req, res) => {
     supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
     developerWhatsapp: process.env.DEVELOPER_WHATSAPP_LINK || '',
     developerGithub: process.env.DEVELOPER_GITHUB_LINK || 'https://github.com/Rahat0764',
-    developerLinkedin: process.env.DEVELOPER_LINKEDIN_LINK || 'https://linkedin.com/in/RahatAhmedX'
+    developerLinkedin: process.env.DEVELOPER_LINKEDIN_LINK || 'https://linkedin.com/in/RahatAhmedX',
+    superAdminEmail: process.env.SUPER_ADMIN_EMAIL || ''
   });
 };
