14c5931fb2080efacfa4c5f47d39fe02800ae143
Rahat Ahmed <87570918+Rahat0764@users.noreply.github.com>
2026-08-12 22:36:32 +0600

    Update config.js

--- api/config.js
+++ api/config.js
@@ -5,6 +5,7 @@ module.exports = (req, res) => {
     developerWhatsapp: process.env.DEVELOPER_WHATSAPP_LINK || '',
     developerGithub: process.env.DEVELOPER_GITHUB_LINK || 'https://github.com/Rahat0764',
     developerLinkedin: process.env.DEVELOPER_LINKEDIN_LINK || 'https://linkedin.com/in/RahatAhmedX',
-    superAdminEmail: process.env.SUPER_ADMIN_EMAIL || ''
+    superAdminEmail: process.env.SUPER_ADMIN_EMAIL || '',
+    apkUrl: process.env.APK_URL || ''
   });
 };
